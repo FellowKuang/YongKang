594d6d18fa694084bf2470c9b8cb136c34b62e71
WirdomK <kuangyk0728@163.com>
2019-02-18 22:52:10 +0800

    CRRC

---
+++ CRRC.sql
@@ -0,0 +1,617 @@
+use crrc;
+
+CREATE TABLE `crrc`.`ccq_pressure01` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure02` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure03` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure04` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure05` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure06` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure07` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure08` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure09` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure10` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure11` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure12` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure13` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure14` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure15` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure16` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure17` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure18` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure19` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure20` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  
+
+CREATE TABLE `crrc`.`cyzd_pressure01` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure02` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure03` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure04` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure05` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure06` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure07` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure08` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure09` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure10` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure11` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure12` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure13` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure14` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure15` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure16` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure17` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure18` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure19` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`cyzd_pressure20` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+
+
+
+CREATE TABLE `crrc`.`ccq_pressure01` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure02` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure03` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure04` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure05` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure06` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure07` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure08` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure09` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure10` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure11` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure12` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure13` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure14` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure15` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure16` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure17` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure18` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure19` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  CREATE TABLE `crrc`.`ccq_pressure20` (
+  `ID` INT NOT NULL,
+  `时间` VARCHAR(45) NOT NULL,
+  `列车管前端` DOUBLE NOT NULL,
+  `副风缸` DOUBLE NOT NULL,
+  `制动缸` DOUBLE NOT NULL,
+  `列车管尾端` DOUBLE NOT NULL,
+  `加缓风缸` DOUBLE NOT NULL,
+  PRIMARY KEY (`ID`));
+  
+  create table `crrc`.`jl_pressure`(
+  `ID` int not null,
+  `位置` int not null,
+  `时间` varchar(45) not null,
+  `列车管前端` double not null,
+  `副风缸` double not null,
+  `制动缸` double not null,
+  `列车管尾端` double not null,
+  `加缓风缸` double not null,
+  primary key(`ID`)
+  );
